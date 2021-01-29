150e003e6a403b294371e478682bdc0e809cd3b1
Anica2802 <anica.mitrovic@yahoo.com>
2021-01-29 20:47:41 +0100

    - Controller modified

---
+++ Commander/Controllers/CommandsController.cs
@@ -0,0 +1,34 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockRepo repos = new MockRepo();
+
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = repos.GetAllCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET request that will responde to URL api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var singleCommandItem = repos.GetCommandById(id);
+
+            return Ok(singleCommandItem);
+        }
+    }
+}
